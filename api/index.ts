2995990d1369658fe91f4997d32b7fccc189ebf6
Thomas Bailey <thomas@photon.garden>
2022-01-26 18:20:19 -0500

    Add dotenv.

--- api/index.ts
+++ api/index.ts
@@ -1,4 +1,7 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node'
+import 'dotenv/config'
+
+const notionApiToken = process.env.NOTION_API_TOKEN
 
 export default function (req: VercelRequest, res: VercelResponse) {
     const { name = 'World' } = req.query;
